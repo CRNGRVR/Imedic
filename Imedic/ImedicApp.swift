372bdbbd0f75c53dd42f5a4251d359fcaa194ca2
Ivan <i.a.konuhov@gmail.com>
2023-02-22 20:30:37 +0700

    Initial Commit

---
+++ Imedic/ImedicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImedicApp.swift
+//  Imedic
+//
+//  Created by Иван on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImedicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
